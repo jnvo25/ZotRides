491a156a863807653b6c2bae7799376add0dd935
Ethan Wong <wongea2@uci.edu>
2021-04-07 13:18:32 -0700

    display car data

--- WebContent/index.js
+++ WebContent/index.js
@@ -17,8 +17,8 @@ function handleStarResult(resultData) {
     console.log("handleStarResult: populating star table from resultData");
 
     // Populate the star table
-    // Find the empty table body by id "star_table_body"
-    let starTableBodyElement = jQuery("#star_table_body");
+    // Find the empty table body by id "car_table_body"
+    let carTableBodyElement = jQuery("#car_table_body");
 
     // Iterate through resultData, no more than 10 entries
     for (let i = 0; i < Math.min(10, resultData.length); i++) {
@@ -26,18 +26,24 @@ function handleStarResult(resultData) {
         // Concatenate the html tags with resultData jsonObject
         let rowHTML = "";
         rowHTML += "<tr>";
+        /*
         rowHTML +=
             "<th>" +
+            /*
             // Add a link to single-star.html with id passed with GET url parameter
             '<a href="single-star.html?id=' + resultData[i]['star_id'] + '">'
             + resultData[i]["star_name"] +     // display star_name for the link text
             '</a>' +
             "</th>";
-        rowHTML += "<th>" + resultData[i]["star_dob"] + "</th>";
+
+                resultData[i]["car_id"];*/
+        rowHTML += "<th>" + resultData[i]["car_model"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["car_make"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["car_year"] + "</th>";
         rowHTML += "</tr>";
 
         // Append the row created to the table body, which will refresh the page
-        starTableBodyElement.append(rowHTML);
+        carTableBodyElement.append(rowHTML);
     }
 }
 
@@ -50,6 +56,6 @@ function handleStarResult(resultData) {
 jQuery.ajax({
     dataType: "json", // Setting return data type
     method: "GET", // Setting request method
-    url: "api/stars", // Setting request url, which is mapped by StarsServlet in Stars.java
+    url: "api/cars", // Setting request url, which is mapped by StarsServlet in Stars.java
     success: (resultData) => handleStarResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
 });
