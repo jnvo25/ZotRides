dfcffa987c3de697a5c0acee4b051e428e30aec7
Ethan Wong <wongea2@uci.edu>
2021-04-21 17:54:24 -0700

    add servlet to handle updating dates of items from shopping cart

---
+++ src/main/java/com/example/zotrides/ModifyCartItemServlet.java
@@ -0,0 +1,88 @@
+package com.example.zotrides;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ * This ModifyCartItemServlet is declared in the web annotation below,
+ * which is mapped to the URL pattern /api/index.
+ */
+@WebServlet(name = "ModifyCartItemServlet", urlPatterns = "/api/modify-cart-item")
+public class ModifyCartItemServlet extends HttpServlet {
+
+    /**
+     * handles GET requests to view shopping cart (called after modifying dates)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+
+        JsonObject responseJsonObject = new JsonObject();
+
+        ArrayList<CartItem> previousItems = (ArrayList<CartItem>) session.getAttribute("previousItems");
+        if (previousItems == null) {
+            previousItems = new ArrayList<>();
+        }
+        JsonArray previousItemsJsonArray = new JsonArray();
+
+        for (CartItem item : previousItems) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("name", item.getCarName());
+            jsonObject.addProperty("pickupLocation", item.getPickupLocation());
+            jsonObject.addProperty("startDate", item.getStartDate());
+            jsonObject.addProperty("endDate", item.getEndDate());
+            jsonObject.addProperty("unitPrice", item.getUnitPrice());
+            jsonObject.addProperty("id", item.getItemID()); // used to uniquely identify cart item
+            previousItemsJsonArray.add(jsonObject);
+        }
+
+        responseJsonObject.add("previousItems", previousItemsJsonArray);
+
+        // write all the data into the jsonObject
+        response.getWriter().write(responseJsonObject.toString());
+    }
+
+    /**
+     * handles POST requests to edit shopping cart item quantity
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // get elements of item to store into shopping cart
+        String startDate = request.getParameter("startDate");
+        String endDate = request.getParameter("endDate");
+        int id = Integer.parseInt(request.getParameter("itemID"));
+        System.out.println("id: " + id + " dates: " + startDate + " " + endDate);
+
+        HttpSession session = request.getSession();
+
+        // get the previous items in a ArrayList, which must already exist
+        ArrayList<CartItem> previousItems = (ArrayList<CartItem>) session.getAttribute("previousItems");
+
+        // prevent corrupted states through sharing under multi-threads
+        // will only be executed by one thread at a time
+        synchronized (previousItems) {
+            for (CartItem item : previousItems) {
+                if (item.getItemID() == id) {
+                    item.setStartDate(startDate);
+                    item.setEndDate(startDate);
+                    break;
+                }
+            }
+        }
+
+        // return updated cart
+        doGet(request, response);
+    }
+}
