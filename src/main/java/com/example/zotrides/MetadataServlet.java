ed0c22d523474caf4bbe846403454aaef51bf9b9
Ethan Wong <wongea2@uci.edu>
2021-05-07 16:45:38 -0700

    metadata employee backend

---
+++ src/main/java/com/example/zotrides/MetadataServlet.java
@@ -0,0 +1,101 @@
+package com.example.zotrides;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet(name = "MetadataServlet", urlPatterns = "/api/metadata")
+public class MetadataServlet extends HttpServlet {
+
+    // Create a dataSource which registered in web.xml
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/zotrides");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // TODO : HAVEN'T TESTED THE SERVLET YET, LMK IF THERE'S SOMETHING WACK
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        /* API
+         *   - Parameters : NONE
+         *   - Return --> {"numTables": "...", 1 : {tableName: "...", "fields": [...], "types": [...]}, ...}
+         *   ---------
+         *   Use the numTables property to determine how many tables' metadata need to be displayed.
+         *   For each i = 1 ... n -th table, it will have an element for its name, a JSON array for all its fields,
+         *     and another JSONArray for all of its fields' types.   */
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        /* run stored procedure */
+        try (Connection conn = dataSource.getConnection()) {
+            // View all tables
+            PreparedStatement statement = conn.prepareStatement("SHOW TABLES;");
+            ResultSet rs = statement.executeQuery();
+
+            // Get metadata for each table
+            PreparedStatement statement2;
+            ResultSet rs2;
+            JsonObject finalResult = new JsonObject();
+            int i;
+            for (i = 0; rs.next(); ++i) {
+                String table = rs.getString("Tables_in_zotrides");
+                statement2 = conn.prepareStatement("DESCRIBE " + table);
+                rs2 = statement2.executeQuery();
+
+                JsonObject result = new JsonObject();
+                JsonArray fields = new JsonArray();
+                JsonArray types = new JsonArray();
+                while(rs2.next()) {
+                    fields.add(rs2.getString("Field"));
+                    types.add(rs2.getString("Type"));
+                }
+                result.addProperty("tableName", table);
+                result.add("fields", fields);
+                result.add("types", types);
+                finalResult.add(Integer.toString(i + 1), result);
+                System.out.println(result.toString()); // DEBUGGING
+                statement2.close();
+                rs2.close();
+            }
+            finalResult.addProperty("numTables", i);
+            out.write(finalResult.toString());
+            rs.close();
+            statement.close();
+            out.close();
+        } catch (Exception e) {
+
+            // write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // for more detail
+            System.out.println(e.getMessage());
+
+            // set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        }
+        out.close();
+    }
+}
---
+++ src/main/java/com/example/zotrides/UpdateSecurePassword.java
@@ -0,0 +1,78 @@
+package com.example.zotrides;
+
+import org.jasypt.util.password.PasswordEncryptor;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+
+public class UpdateSecurePassword {
+
+    /*
+     * 
+     * This program updates the existing zotrides customers table to change the
+     * plain text passwords to encrypted passwords.
+     * 
+     * This program should only be run once **once**, because this program uses the
+     * existing passwords as real passwords, then replace them. If you run it more
+     * than once, it will treat the encrypted passwords as real passwords and
+     * generate wrong values.
+     * 
+     */
+    public static void main(String[] args) throws Exception {
+        // Setup for connection
+        String loginUser = "mytestuser";
+        String loginPasswd = "My6$Password";
+        String loginUrl = "jdbc:mysql://localhost:3306/zotrides";
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+        // Establish connection
+        try(Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);) {
+            // get the ID and password for each customer
+            PreparedStatement statement = connection.prepareStatement("SELECT id, password from Customers");
+            ResultSet rs = statement.executeQuery();
+
+            // we use the StrongPasswordEncryptor from jasypt library (Java Simplified Encryption)
+            //  it internally use SHA-256 algorithm and 10,000 iterations to calculate the encrypted password
+            PasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
+
+            // execute the update queries to update the passwords
+            String encryptQuery = "UPDATE Customers SET password=? WHERE id=?;";
+            PreparedStatement statement2 = connection.prepareStatement(encryptQuery);
+            System.out.println("encrypting password (this might take a while)");
+            int count = 0;
+            while (rs.next()) {
+                // get the ID and plain text password from current table
+                String id = rs.getString("id");
+                String password = rs.getString("password");
+
+                // encrypt the password using StrongPasswordEncryptor
+                String encryptedPassword = passwordEncryptor.encryptPassword(password);
+
+                // generate the update query
+                statement2.setString(1, encryptedPassword);
+                statement2.setString(2, id);
+                count += statement2.executeUpdate();
+            }
+
+            System.out.println("updating password completed, " + count + " rows affected");
+
+            // clean up
+            rs.close();
+            statement.close();
+            statement2.close();
+            connection.close();
+
+            System.out.println("finished");
+
+        } catch (Exception e) {
+            // display error message
+            System.out.println("Error: " + e.getMessage());
+            return;
+        }
+
+    }
+
+}
