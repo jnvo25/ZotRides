cfcac9406c0e70186db5b32ccc8400fb0a28f6d5
Ethan Wong <wongea2@uci.edu>
2021-05-19 16:04:27 -0700

    app setup

---
+++ mobile-app/app/src/main/java/edu/uci/ics/fabflixmobile/ListViewActivity.java
@@ -0,0 +1,33 @@
+package edu.uci.ics.fabflixmobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ListViewActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview);
+        // TODO: this should be retrieved from the backend server
+        final ArrayList<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("The Terminal", (short) 2004));
+        movies.add(new Movie("The Final Season", (short) 2007));
+
+        /* adapter object used to display list */
+        MovieListViewAdapter adapter = new MovieListViewAdapter(movies, this);
+
+        ListView listView = findViewById(R.id.list); // list id corresponds to UI's list id in listview.xml
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Movie movie = movies.get(position);
+            String message = String.format("Clicked on position: %d, name: %s, %d", position, movie.getName(), movie.getYear());
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+            /* Toast class to display message */
+        });
+    }
+}
