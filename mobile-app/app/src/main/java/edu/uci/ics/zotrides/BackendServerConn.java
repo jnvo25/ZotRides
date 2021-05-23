7edfbc7016de75de2dc06d45c49ad93bf520ddfc
Ethan Wong <wongea2@uci.edu>
2021-05-22 21:55:44 -0700

    change IP for deploymente

--- mobile-app/app/src/main/java/edu/uci/ics/zotrides/BackendServerConn.java
+++ mobile-app/app/src/main/java/edu/uci/ics/zotrides/BackendServerConn.java
@@ -1,7 +1,7 @@
 package edu.uci.ics.zotrides;
 
 public class BackendServerConn {
-    private static final String host = "10.0.2.2"; // IP address for localhost
+    private static final String host = "18.217.144.182"; // IP address for localhost
     private static final String port = "8443";
     private static final String domain = "ZotRides";
 
