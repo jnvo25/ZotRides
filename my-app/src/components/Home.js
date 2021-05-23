0e3d8628d4e7cd06048a04697dfc0423773d23fb
jonathanvo <jnvo25@gmail.com>
2021-05-22 21:14:48 -0700

    implement working delay

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {Button, Col, Row, Jumbotron, Container} from 'react-bootstrap';
 import {Typeahead} from "react-bootstrap-typeahead";
 import './stylesheets/Home.css';
@@ -14,42 +14,49 @@ export default function Home() {
     const [redirectId, setRedirectId] = useState();
     const [redirect, setRedirect] = useState(false);
     const [cache, setCache] = useState({});
-    const [index, setIndex] = useState([]);
+    const [activeIndex, setActiveIndex] = useState(-1);
 
-    function onChange(input) {
-        setText(input);
+    useEffect(() => {
+        const timeOutId = setTimeout(() => runQuery(text), 300);
+        return () => clearTimeout(timeOutId);
+    }, [text]);
+
+    function runQuery(input) {
         var match = "";
         if(input.length >= 3 && Object.keys(cache).some((element) => {
             match = element;
-            return ~element.indexOf(input)
+            return ~element.indexOf(input.toLowerCase());
         })) {
-            console.log("From cache");
+            console.log("Autocomplete options populated from cache");
             setOptions(cache[match]);
+            console.log("CACHE CONTENTS:");
+            console.log(cache);
         } else if(input.length >= 3) {
-            console.log("Autocomplete initiated");
+            console.log("Autocomplete options populated from database");
             jQuery.ajax({
                 dataType: "json",
                 method: "POST",
-                data: {token: input},
+                data: {token: input.toLowerCase()},
                 url: HOST + "api/autocomplete",
                 success: (resultData) => {
                     setOptions(resultData.results);
-                    // Add to cache
                     const temp = cache;
                     temp[input] = resultData.results;
+                    console.log("Added to cache")
                     setCache(temp);
-                    // Keep track of order cache is populated
-                    const indexTemp = index;
-                    indexTemp.push(input);
-                    setIndex(indexTemp);
+                    console.log("CACHE CONTENTS:");
+                    console.log(cache);
                 }
             });
         } else {
             setOptions([]);
         }
     }
+
+    function onChange(input) {
+        setText(input.toLowerCase());
+    }
     function onSelect(input) {
-        console.log(input);
         setSingleSelections(input);
         options.map(element => {
             if(input[0] === element["car_name"])
@@ -58,24 +65,9 @@ export default function Home() {
         setSuccess(true);
     }
 
-    function onKeyDown(e) {
-        if(e.key === "Enter" && text.length !== 0) {
+    function handleKey(e) {
+        if (e.key === "Enter" && activeIndex === -1) {
             setRedirect(true);
-        }
-    }
-
-    if (index.length > 99) {
-        console.log("deleting")
-        const rElement = index[0];
-        var match = "";
-
-        Object.keys(cache).some((element) => {
-            match = element;
-            return ~element.indexOf(rElement)
-        })
-        if (match !== "") {
-            index.shift();
-            delete cache[match];
         }
     }
 
@@ -94,15 +86,20 @@ export default function Home() {
                             <Typeahead
                                 onInputChange={onChange}
                                 delay={300}
-                                onKeyDown={onKeyDown}
                                 id="basic-typeahead-single"
                                 labelKey="name"
                                 onChange={onSelect}
                                 options={options.map((element => {return element["car_name"]}))}
                                 placeholder="Enter model name..."
                                 selected={singleSelections}
-
-                            />
+                                onKeyDown={handleKey}
+                            >
+                                {(state) => {
+                                    // Passing a child render function to the component exposes partial
+                                    // internal state, including the index of the highlighted menu item.
+                                    setActiveIndex(state.activeIndex);
+                                }}
+                            </Typeahead>
                         </Col>
                         <Col xs={1}>
                             <Button onClick={()=>{if(text.length !== 0) setRedirect(true)}}>Search</Button>
